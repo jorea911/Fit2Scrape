802de75827e81d92267a213ac46504202f028701
jeffrey <jorea911@gmail.com>
2019-04-24 13:12:13 -0700

    heroku 2

--- server.js
+++ server.js
@@ -125,6 +125,6 @@ app.put("/update/articles/:id",function(req,res){
 });
     
 // Start the server
-app.listen(PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("App running on port " + PORT + "!");
 });
